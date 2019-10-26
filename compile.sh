1d6e787a1f8e0401b2945f5377fe1b04ee915195
Braden Pezeshki <braden.pezeshki@gmail.com>
2019-10-26 12:57:00 -0700

    :building_construction: added postinstall

--- compile.sh
+++ compile.sh
@@ -12,7 +12,7 @@ fi
 mkdir -p abi && rm -r abi
 mkdir -p abi/bin abi/json
 
-solc --abi --bin -o abi --allow-paths . @openzeppelin=./node_modules/@openzeppelin contracts/**/*.sol --optimize --optimize-runs 200 >/dev/null
+solc --abi --bin -o abi --allow-paths . @openzeppelin=${INIT_CWD:-.}/node_modules/@openzeppelin contracts/**/*.sol --optimize --optimize-runs 200 >/dev/null
 
 find abi -name '*.bin' -type f -size 0 -empty -delete
 find abi -name '*.bin' -exec sh -c 'mv "$0" abi/bin 2>/dev/null' {} \;
