8934fcdcd40ce120a7e675d51ab4211ec07b6c20
Braden Pezeshki <braden.pezeshki@gmail.com>
2019-11-05 09:19:07 -0800

    :art: updated signature resolver

--- contracts/resolver/SignatureResolver.sol
+++ contracts/resolver/SignatureResolver.sol
@@ -2,25 +2,13 @@ pragma solidity 0.5.11;
 
 import './Resolver.sol';
 import '@openzeppelin/contracts/cryptography/ECDSA.sol';
+import '../util/SignatureUtil.sol';
 
 // solium-disable error-reason
 
-contract SignatureResolver is Resolver {
-    using ECDSA for bytes32;
+contract SignatureResolver is Resolver, SignatureUtil {
 
-    // Mapping from owner to a nonce
-    mapping (address => uint256) internal _nonces;
-
-    constructor(Registry registry) public Resolver(registry) {}
-
-    /**
-     * @dev Gets the nonce of the specified address.
-     * @param addr address to query nonce for
-     * @return nonce of the given address
-     */
-    function nonceOf(address addr) external view returns (uint256) {
-        return _nonces[addr];
-    }
+    constructor(Registry registry) public Resolver(registry) SignatureUtil(registry) {}
 
     /**
      * @dev Function to set record on behalf of an address.
@@ -30,23 +18,9 @@ contract SignatureResolver is Resolver {
      * @param signature The signature to verify the transaction with.
      */
     function setFor(bytes calldata key, bytes calldata value, uint256 tokenId, bytes calldata signature) external whenResolver(tokenId) {
-        address owner = _registry.ownerOf(tokenId);
-        _validate(keccak256(abi.encodeWithSelector(msg.sig, key, value, tokenId)), tokenId, signature);
+        _validate(keccak256(abi.encodeWithSelector(this.set.selector, key, value, tokenId)), tokenId, signature);
          _registry.sync(tokenId, uint256(keccak256(key)));
-        _set(owner, key, value, tokenId);
+        _set(_registry.ownerOf(tokenId), key, value, tokenId);
     }
 
-    function _validate(bytes32 hash, uint256 tokenId, bytes memory signature) internal {
-        address owner = _registry.ownerOf(tokenId);
-        uint256 nonce = _nonces[owner];
-
-        require(
-            _registry.isApprovedOrOwner(
-                keccak256(abi.encodePacked(hash, nonce)).toEthSignedMessageHash().recover(signature),
-                tokenId
-            )
-        );
-
-        _nonces[owner] += 1;
-    }
 }
