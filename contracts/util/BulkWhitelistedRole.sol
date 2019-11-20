7ace1ced5bb2c93d91c405549d7f877ad0d9fec1
Braden Pezeshki <braden.pezeshki@gmail.com>
2019-11-20 11:27:22 -0800

    :art: inherit from WhitelistedRole

--- contracts/util/BulkWhitelistedRole.sol
+++ contracts/util/BulkWhitelistedRole.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.12;
 
 import "@openzeppelin/contracts/access/Roles.sol";
-import "@openzeppelin/contracts/access/roles/WhitelistAdminRole.sol";
+import "@openzeppelin/contracts/access/roles/WhitelistedRole.sol";
 
 /**
  * @title BulkWhitelistedRole
@@ -11,35 +11,12 @@ import "@openzeppelin/contracts/access/roles/WhitelistAdminRole.sol";
  * crowdsale). This role is special in that the only accounts that can add it are WhitelistAdmins (who can also remove
  * it), and not Whitelisteds themselves.
  */
-contract BulkWhitelistedRole is WhitelistAdminRole {
-    using Roles for Roles.Role;
-
-    event WhitelistedAdded(address indexed account);
-    event WhitelistedRemoved(address indexed account);
-
-    Roles.Role private _whitelisteds;
-
-    modifier onlyWhitelisted() {
-        require(isWhitelisted(msg.sender), "BulkWhitelistedRole: caller does not have the Whitelisted role");
-        _;
-    }
-
-    function isWhitelisted(address account) public view returns (bool) {
-        return _whitelisteds.has(account);
-    }
-
-    function addWhitelisted(address account) public onlyWhitelistAdmin {
-        _addWhitelisted(account);
-    }
+contract BulkWhitelistedRole is WhitelistedRole {
 
     function bulkAddWhitelisted(address[] memory accounts) public onlyWhitelistAdmin {
         for (uint256 index = 0; index < accounts.length; index++) {
             _addWhitelisted(accounts[index]);
         }
-    }
-
-    function removeWhitelisted(address account) public onlyWhitelistAdmin {
-        _removeWhitelisted(account);
     }
 
     function bulkRemoveWhitelisted(address[] memory accounts) public onlyWhitelistAdmin {
@@ -48,17 +25,4 @@ contract BulkWhitelistedRole is WhitelistAdminRole {
         }
     }
 
-    function renounceWhitelisted() public {
-        _removeWhitelisted(msg.sender);
-    }
-
-    function _addWhitelisted(address account) internal {
-        _whitelisteds.add(account);
-        emit WhitelistedAdded(account);
-    }
-
-    function _removeWhitelisted(address account) internal {
-        _whitelisteds.remove(account);
-        emit WhitelistedRemoved(account);
-    }
 }
