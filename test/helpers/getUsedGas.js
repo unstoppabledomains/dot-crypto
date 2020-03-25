e7bda5f1ae516311eb7f89aac63324dbac075122
Kirill Beresnev <derainberk@gmail.com>
2020-03-25 15:26:22 +0300

    Fix comment

--- test/helpers/getUsedGas.js
+++ test/helpers/getUsedGas.js
@@ -1,6 +1,6 @@
 // All estimates are using common gas price and eth to usd rate.
 const gasPrice = 2 * Math.pow(10, 9); // 2 GWei
-const ethToUsdRate = 140; // 1 ETH = $150
+const ethToUsdRate = 140; // 1 ETH = $140
 
 module.exports = {
     getUsedGas: (tx) => `${ tx.receipt.gasUsed } gas (~$${
