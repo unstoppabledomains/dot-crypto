d6675af94da312a8a66cc592436bc0d172ecf167
Sergii Bomko <aquila.ua@gmail.com>
2020-08-18 16:37:11 +0300

    fix ganache signature

--- test/helpers/submitSigTransaction.js
+++ test/helpers/submitSigTransaction.js
@@ -25,7 +25,19 @@ async function submitSigTransaction(contractToSend, contractToEncode, fromAccoun
       fromAccount,
     )
 
-    return contractToSend[method + 'For'](...args, signature)
+    return contractToSend[method + 'For'](...args, fixSignature(signature))
   }
+
+function fixSignature (signature) {
+  // in geth its always 27/28, in ganache its 0/1. Change to 27/28 to prevent
+  // signature malleability if version is 0/1
+  // see https://github.com/ethereum/go-ethereum/blob/v1.8.23/internal/ethapi/api.go#L465
+  let v = parseInt(signature.slice(130, 132), 16);
+  if (v < 27) {
+    v += 27;
+  }
+  const vHex = v.toString(16);
+  return signature.slice(0, 130) + vHex;
+}
 
 module.exports = submitSigTransaction
