ab854ede6a835bd938a4c6c71c05825a0903c196
Sergii Bomko <aquila.ua@gmail.com>
2020-09-14 16:57:09 +0300

    fix security issue, add close/rotate whitelisted

--- contracts/util/WhitelistedMinter.sol
+++ contracts/util/WhitelistedMinter.sol
@@ -12,49 +12,129 @@ import "../Resolver.sol";
  * @dev Defines the functions for distribution of Second Level Domains (SLD)s.
  */
 contract WhitelistedMinter is IMintingController, BulkWhitelistedRole {
-
     MintingController internal _mintingController;
     Resolver internal _resolver;
     Registry internal _registry;
 
-    constructor (MintingController mintingController) public {
+    constructor(MintingController mintingController) public {
         _mintingController = mintingController;
         _registry = Registry(mintingController.registry());
     }
 
-    function renounceMinter() external {
+    function renounceMinter() external onlyWhitelistAdmin {
         _mintingController.renounceMinter();
     }
 
-    function mintSLD(address to, string calldata label) external onlyWhitelisted {
+    /**
+     * Renounce whitelisted account with funds' forwarding
+     */
+    function closeWhitelisted(address payable receiver)
+        external
+        payable
+        onlyWhitelisted
+    {
+        require(
+            receiver != address(0x0),
+            "WhitelistedMinter: receiver must be non-zero."
+        );
+
+        renounceWhitelisted();
+        receiver.transfer(msg.value);
+    }
+
+    /**
+     * Replace whitelisted account by new account with funds' forwarding
+     */
+    function rotateWhitelisted(address payable receiver)
+        external
+        payable
+        onlyWhitelisted
+    {
+        require(
+            receiver != address(0x0),
+            "WhitelistedMinter: receiver must be non-zero."
+        );
+
+        _addWhitelisted(receiver);
+        renounceWhitelisted();
+        receiver.transfer(msg.value);
+    }
+
+    function mintSLD(address to, string calldata label)
+        external
+        onlyWhitelisted
+    {
         _mintingController.mintSLD(to, label);
     }
 
-    function safeMintSLD(address to, string calldata label) external onlyWhitelisted {
+    function safeMintSLD(address to, string calldata label)
+        external
+        onlyWhitelisted
+    {
         _mintingController.safeMintSLD(to, label);
     }
 
-    function safeMintSLD(address to, string calldata label, bytes calldata _data) external onlyWhitelisted {
+    function safeMintSLD(
+        address to,
+        string calldata label,
+        bytes calldata _data
+    ) external onlyWhitelisted {
         _mintingController.safeMintSLD(to, label, _data);
     }
 
-    function mintSLDToDefaultResolver(address to, string memory label, string[] memory keys, string[] memory values) public onlyWhitelisted {
+    function mintSLDToDefaultResolver(
+        address to,
+        string memory label,
+        string[] memory keys,
+        string[] memory values
+    ) public onlyWhitelisted {
         _mintingController.mintSLDWithResolver(to, label, address(_resolver));
-        _resolver.preconfigure(keys, values, _registry.childIdOf(_registry.root(), label));
+        _resolver.preconfigure(
+            keys,
+            values,
+            _registry.childIdOf(_registry.root(), label)
+        );
     }
 
-    function safeMintSLDToDefaultResolver(address to, string memory label, string[] memory keys, string[] memory values) public onlyWhitelisted {
-        _mintingController.safeMintSLDWithResolver(to, label, address(_resolver));
-        _resolver.preconfigure(keys, values, _registry.childIdOf(_registry.root(), label));
+    function safeMintSLDToDefaultResolver(
+        address to,
+        string memory label,
+        string[] memory keys,
+        string[] memory values
+    ) public onlyWhitelisted {
+        _mintingController.safeMintSLDWithResolver(
+            to,
+            label,
+            address(_resolver)
+        );
+        _resolver.preconfigure(
+            keys,
+            values,
+            _registry.childIdOf(_registry.root(), label)
+        );
     }
 
-    function safeMintSLDToDefaultResolver(address to, string memory label, string[] memory keys, string[] memory values, bytes memory _data) public onlyWhitelisted {
-        _mintingController.safeMintSLDWithResolver(to, label, address(_resolver), _data);
-        _resolver.preconfigure(keys, values, _registry.childIdOf(_registry.root(), label));
+    function safeMintSLDToDefaultResolver(
+        address to,
+        string memory label,
+        string[] memory keys,
+        string[] memory values,
+        bytes memory _data
+    ) public onlyWhitelisted {
+        _mintingController.safeMintSLDWithResolver(
+            to,
+            label,
+            address(_resolver),
+            _data
+        );
+        _resolver.preconfigure(
+            keys,
+            values,
+            _registry.childIdOf(_registry.root(), label)
+        );
     }
 
     function setDefaultResolver(address resolver) external onlyWhitelistAdmin {
         _resolver = Resolver(resolver);
     }
-
 }
