e3870b1fac693230aee8e098ad615da8c22a7098
Hennadii Ivtushok <hennadii@unstoppabledomains.com>
2020-10-22 11:31:47 +0300

    Introduce network config file

---
+++ src/network-config/network-config.ts
@@ -0,0 +1,21 @@
+import json from './network-config.json';
+
+type ChainId = number;
+
+type ContractName = 'Registry' | 'SignatureController' | 'MintingController' | 'WhitelistedMinter'
+    | 'URIPrefixController' | 'DomainZoneController' | 'Resolver' | 'ProxyReader' | 'TwitterValidationOperator';
+
+type Address = string;
+
+type NetworkConfig = Readonly<{
+    contracts: Readonly<Record<ContractName, { address: Address, legacyAddresses: ReadonlyArray<Address> }>>,
+}>;
+
+type Config = Readonly<{
+    version: string,
+    networks: Readonly<Record<ChainId, NetworkConfig>>,
+}>;
+
+const configCheck: Config = json;
+
+export const ContractsConfig = json;
