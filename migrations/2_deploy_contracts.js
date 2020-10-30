fbd5bd0f9a2c26eb2a37c34956bd7ee8e35f3168
Sergii Bomko <aquila.ua@gmail.com>
2020-10-30 18:55:52 +0300

    update contracts migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,17 +6,33 @@ const SignatureController = artifacts.require(
   'controller/SignatureController.sol',
 )
 const MintingController = artifacts.require('controller/MintingController.sol')
+const DomainZoneController = artifacts.require('controller/DomainZoneController.sol')
 const WhitelistedMinter = artifacts.require('util/WhitelistedMinter.sol')
 const Resolver = artifacts.require('Resolver.sol')
-const Simple = artifacts.require('test-helpers/Simple.sol')
 
-module.exports = (deployer, network, accounts) => {
+const rinkebyWorkers = [
+  '0xb3B86785A51B950fd54ABdF420ff3B60E091870c',
+  '0x7EF88A779651f26a4967026a32Cae4F01fF8D151',
+  '0x0c71a3494484459bbF9777Dd3109515B2E653CCb',
+  '0x288DA3443BBEBcc7a339182323aa3F23126DFe7a',
+  '0xe45541799119C1D63b60e0F834F3A381D4BEDbea',
+];
+
+const rinkebyPriorityWorkers = [
+  '0xdb36B5c4cF1D96f020D7629a09cB54ab787414d6',
+  '0x3b9FB7983d897B7fe2fD7563e07e24CbA830b03B',
+  '0x903aA579B9eF13862Fda73275B349017d8fD09eB',
+  '0x7Ac8596cfbb0504DFDEC08d5088B67E7fbfae47f',
+  '0xB83180632b72f988585AF02FC27229bF2Eabd139',
+];
+
+module.exports = (deployer, network) => {
   deployer.deploy(Registry).then(async registry => {
     const signatureController = await deployer.deploy(
       SignatureController,
       registry.address,
     )
-    const sunriseController = await deployer.deploy(
+    const mintingController = await deployer.deploy(
       MintingController,
       registry.address,
     )
@@ -24,9 +40,14 @@ module.exports = (deployer, network, accounts) => {
       URIPrefixController,
       registry.address,
     )
+    const domainZoneController = await deployer.deploy(
+      DomainZoneController,
+      registry.address,
+      []
+    )
 
     await registry.addController(signatureController.address)
-    await registry.addController(sunriseController.address)
+    await registry.addController(mintingController.address)
     await registry.addController(uriPrefixController.address)
 
     if (network === 'live') {
@@ -35,17 +56,23 @@ module.exports = (deployer, network, accounts) => {
 
     const whitelistedMinter = await deployer.deploy(
       WhitelistedMinter,
-      sunriseController.address,
+      mintingController.address,
     )
-    await whitelistedMinter.addWhitelisted(accounts[0])
-    await sunriseController.addMinter(whitelistedMinter.address)
+    await mintingController.addMinter(whitelistedMinter.address)
 
-    if (network === 'live') {
-      await sunriseController.renounceMinter()
+    if (network === 'rinkeby') {
+      await whitelistedMinter.bulkAddWhitelisted([
+        ...rinkebyWorkers,
+        ...rinkebyPriorityWorkers
+      ])
+      await domainZoneController.bulkAddWhitelisted(rinkebyPriorityWorkers)
     }
 
-    await deployer.deploy(Resolver, registry.address, sunriseController.address)
-    await deployer.deploy(Simple)
+    if (network === 'live') {
+      await mintingController.renounceMinter()
+    }
+
+    await deployer.deploy(Resolver, registry.address, mintingController.address)
 
     console.log('Migrated!')
   })
