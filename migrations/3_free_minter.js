5141e30b27d3cc22e1697aaff7339ffeada46a29
Kirill Beresnev <DeRain@users.noreply.github.com>
2020-11-23 20:01:14 +0300

    Update migrations/3_free_minter.js
    Co-authored-by: Vladyslav Batyrenko <mvlabat@gmail.com>

--- migrations/3_free_minter.js
+++ migrations/3_free_minter.js
@@ -4,7 +4,7 @@ const Resolver = artifacts.require('Resolver.sol')
 const FreeMinter = artifacts.require('util/FreeMinter.sol')
 
 module.exports = async (deployer) => {
-    const registry = await Registry.deployed();
+    const registry = await Registry.deployed()
     const mintingController = await MintingController.deployed()
     const resolver = await Resolver.deployed()
     const freeDomainsMinter = await deployer.deploy(FreeMinter, mintingController.address, resolver.address, registry.address)
