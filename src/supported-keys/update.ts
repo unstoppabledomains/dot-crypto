4fc1133f9b3a7d29583d7b2d6e2feac42a5f7a32
Gentelman John <jeyhunt@gmail.com>
2021-06-10 00:07:58 -0700

    updated script

--- src/supported-keys/update.ts
+++ src/supported-keys/update.ts
@@ -2,15 +2,19 @@ import SupportedKeys  from "./supported-keys.json";
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-type CoinGeckoAllCoinsResponse = {
-  id: string;
-  symbol: string;
-  name: string;
+
+type CoinGeckoCoin = {
+  id: string,
+  symbol: string,
+  name: string
+};
+
+type CoinGeckoDetailCoin = CoinGeckoCoin & {
   platforms: {
     [key in string]: string
   }
 }
-type CoinGeckoCoinDetailResponse = CoinGeckoAllCoinsResponse & { coingecko_score: number }
+
 type CoinGeckoPlatformDetail = {
   id: string,
   chain_identifier: number,
@@ -18,79 +22,53 @@ type CoinGeckoPlatformDetail = {
   shortname: string
 }
 
+type MultiChainMeta = {
+  key: string;
+  deprecatedKeyName: string;
+  version: string;
+  validationRegex: string | undefined;
+}
+
+
 const main = async (): Promise<void> => {
-  const coins = await getFilteredCoins(200);
+  const coins = await getFilteredCoins();
   const assetPlatforms = await getAssetPlatformsList();
   
   const updatedSuppKeys = getUpdatedKeysFromCoins(coins, assetPlatforms);
+  console.log(`Was ${Object.keys(SupportedKeys.keys).length} supported keys records`);
+  console.log(`Become ${Object.keys(updatedSuppKeys).length} supported keys records`);
+
   saveUpdates(updatedSuppKeys);
 }
 
-const getUpdatedKeysFromCoins = (coins: CoinGeckoCoinDetailResponse[], assetPlatforms: CoinGeckoPlatformDetail[]): any => {
-  let updatedKeys = {...SupportedKeys.keys};
-  for (const coin of coins) {
-    if (isMultiChainCoin(coin)) {
-      const multiChainKeys = getMultiChainKeys(coin, assetPlatforms);
-      multiChainKeys.forEach(key => {
-        if (!updatedKeys[key]) {
-          updatedKeys[key] = {
-            "deprecatedKeyName": null,
-            "validationRegex": key.includes('ERC20') ? "^0x[a-fA-F0-9]{40}$" : null,
-            "deprecated": false
-          }
-          console.log(`added the following: ${key}`);
-        }
-      })
-    } else {
-      const key = `crypto.${coin.symbol.toUpperCase()}.address`;
-      if (!updatedKeys[key]) {
-        updatedKeys[key] = {
-          "deprecatedKeyName": null,
-          "validationRegex": coin.platforms['ethereum'] ? "^0x[a-fA-F0-9]{40}$" : null,
-          "deprecated": false
-        }
-        console.log(`added the following: ${key}`);
-      }
-    }
-  }
-  return updatedKeys;
+const getFilteredCoins = async (): Promise<CoinGeckoDetailCoin[]> => {
+  const coins = await getCoinsSortedByScore();
+  const coinsListWithPlatforms = await getAllCoinsWithPlatforms();
+  return coins.map(coin => coinsListWithPlatforms[coin.id]);
 }
 
-const isMultiChainCoin = (coin: CoinGeckoCoinDetailResponse): boolean => {
-  return Object.keys(coin.platforms).length > 1
-} 
-
-const getMultiChainKeys = (coin: CoinGeckoCoinDetailResponse, assetPlatforms: CoinGeckoPlatformDetail[]): string[] => {
-  const keys: string[] = [];
-  for (const [platformId, _] of Object.entries(coin.platforms)) {
-    const platform = assetPlatforms.find(platformDetail => platformDetail.id === platformId);
-    if (!platform) {
-      console.error(`Platform ${platformId} didn't match platforms from CoinGecko`)
-      continue ;
-    }
-
-    let version = platform.shortname
-      ? platform.shortname.replace(/\s/g, "").toUpperCase()
-      : platform.name.replace(/\s/g, "").toUpperCase()
-    version = version === 'ETH' ? 'ERC20' : version;
-
-    keys.push(`crypto.${coin.symbol.toUpperCase()}.version.${version}.address`);
+const getCoinsSortedByScore = async (): Promise<CoinGeckoCoin[]> => {
+  const params = 'vs_currency=usd&order=gecko_desc&per_page=200&page=1&sparkline=false';
+  const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?${params}`);
+  if (response.status !== 200) {
+    console.error(await response.json());
+    throw new Error('Fetch Error on getting top 200 coins');
   }
-  return keys;
+  return response.json();
 }
 
-const saveUpdates = (updatedKeys: {
-  [key in string]: {
-    deprecatedKeyName: string, validationRegex: string | null, deprecated: boolean
-  }}) => {
-  const versionParts = SupportedKeys.version.split('.');
-  versionParts[2] = (Number(versionParts[2]) + 1).toString();
-  const version = versionParts.join('.');
-  const newFile = {
-    version: version,
-    keys: updatedKeys
-  };
-  fs.writeFileSync("./src/supported-keys/supported-keys.json", JSON.stringify(newFile));
+const getAllCoinsWithPlatforms = async (): Promise<{[key in string]: CoinGeckoDetailCoin}> => {
+  const response = await fetch("https://api.coingecko.com/api/v3/coins/list?include_platform=true");
+  if (response.status !== 200) {
+    console.error(await response.json());
+    throw new Error('Fetch Error on getting all coins with platforms');
+  }
+  const coinList: CoinGeckoDetailCoin[] = await response.json();
+  const coinsMap: {[key in string]: CoinGeckoDetailCoin} = {};
+  coinList.forEach(coin => {
+    coinsMap[coin.id] = coin;
+  });
+  return coinsMap;
 }
 
 const getAssetPlatformsList = async (): Promise<CoinGeckoPlatformDetail[]> => {
@@ -106,7 +84,7 @@ const getAssetPlatformsList = async (): Promise<CoinGeckoPlatformDetail[]> => {
       case 'ethereum':
         return {
           ...platform,
-          shortname: 'ETH'
+          shortname: 'ERC20'
         }
       case 'zilliqa':
         return {
@@ -118,51 +96,123 @@ const getAssetPlatformsList = async (): Promise<CoinGeckoPlatformDetail[]> => {
           ...platform,
           shortname: 'ETC'
         }
+      case 'binancecoin':
+        return {
+          ...platform,
+          shortname: 'BEP2'
+        }
+      case 'binance-smart-chain':
+        return {
+          ...platform,
+          shortname: 'BEP20'
+        }
       default: 
         return platform
     }
   })
 }
 
-const getFilteredCoins = async (upTo?: number): Promise<CoinGeckoCoinDetailResponse[]> => {
-  const coins = await getAllCoins();
-  const coinsWithRating: CoinGeckoCoinDetailResponse[] =  []
-  console.log("Fetching coins details");
+
+const getUpdatedKeysFromCoins = (coins: CoinGeckoDetailCoin[], assetPlatforms: CoinGeckoPlatformDetail[]): any => {
+  let updatedKeys = {...SupportedKeys.keys};
   for (const coin of coins) {
-    const details = await getCoinDetails(coin.id);
-    coinsWithRating.push(details);
-    // Due to CoinGecko limit rate we make 2 request and wait 1.5 second
-    if (coinsWithRating.length % 2 === 0) {
-      console.log(`Progress: ${(( coinsWithRating.length / coins.length ) * 100).toPrecision(4)}%`);
-      await delay(1500);
+    if (isMultiChainCoin(coin)) {
+      const multiChainMeta = getMultiChainMeta(coin, assetPlatforms);
+      multiChainMeta.forEach(metaData => {
+        if (updatedKeys[metaData.key] !== undefined) {
+          return;
+        }
+
+        updatedKeys[metaData.key] = {
+          "deprecatedKeyName": metaData.deprecatedKeyName,
+          "validationRegex": metaData.validationRegex ?? null,
+          "deprecated": false
+        }
+      })
+    } else {
+      const key = `crypto.${coin.symbol.toUpperCase()}.address`;
+      if (updatedKeys[key] !== undefined) {
+        continue;
+      }
+
+      const platformId = Object.keys(coin.platforms)[0];
+      const platform = getPlatform(platformId, assetPlatforms);
+      const version = platform ? getVersion(platform) : '';
+      const validationRegex = getValidationRegex(version);
+      
+      updatedKeys[key] = {
+        "deprecatedKeyName": coin.symbol.toUpperCase(),
+        "validationRegex": validationRegex ?? null,
+        "deprecated": false
+      }
     }
   }
-  coinsWithRating.sort((coinA, coinB) => coinB.coingecko_score - coinA.coingecko_score);
-  return coinsWithRating.splice(0, upTo);
+  return updatedKeys;
 }
 
-const getAllCoins = async (): Promise<CoinGeckoAllCoinsResponse[]> => {
-  console.log("Fetching coins");
-  const response = await fetch("https://api.coingecko.com/api/v3/coins/list?include_platform=true");
-  if (response.status !== 200) {
-    console.error(await response.json());
-    throw new Error('Fetch Error on getting all coins');
+const isMultiChainCoin = (coin: CoinGeckoDetailCoin): boolean => {
+  return Object.keys(coin.platforms).length > 1
+} 
+
+const getMultiChainMeta = (coin: CoinGeckoDetailCoin, assetPlatforms: CoinGeckoPlatformDetail[]): MultiChainMeta[] => {
+  const parseResult: MultiChainMeta[] = [];
+  for (const [platformId, _] of Object.entries(coin.platforms)) {
+    const platform = getPlatform(platformId, assetPlatforms);
+    if (!platform) {
+      console.error(`Platform ${platformId} didn't match platforms from CoinGecko`)
+      continue ;
+    }
+    const symbol = coin.symbol.toUpperCase();
+    const version = getVersion(platform);
+    const validationRegex = getValidationRegex(version);
+
+    parseResult.push({
+      key: `crypto.${symbol}.version.${version}.address`,
+      deprecatedKeyName: `${symbol}_${version}`,
+      version,
+      validationRegex
+    });
   }
-  return response.json();
+  return parseResult;
 }
 
-const getCoinDetails = async (coinId: string): Promise<CoinGeckoCoinDetailResponse> => {
-  const params = 'localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false';
-  const response = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}?${params}`);
-  if (response.status !== 200) {
-    console.error(await response.json());
-    throw new Error(`Fetch Error on getting coin ${coinId} details`);
+const getPlatform = (platformId: string, assetPlatforms: CoinGeckoPlatformDetail[]): CoinGeckoPlatformDetail | undefined => {
+  return assetPlatforms.find(platformDetail => platformDetail.id === platformId);
+}
+
+const getVersion = (platform: CoinGeckoPlatformDetail): string => {
+  return platform.shortname
+  ? platform.shortname.replace(/\s/g, "").toUpperCase()
+  : platform.name.replace(/\s/g, "").toUpperCase();
+}
+
+const getValidationRegex = (version: string): string | undefined => {
+  const versionToRegexMap = {
+    'OMNI': '^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$',
+    'EOS': '^[a-z][a-z1-5.]{10}[a-z1-5]$',
+    'BEP2': '^(bnb|tbnb)[a-zA-HJ-NP-Z0-9]{39}$',
+    'TRON': '^[T][a-zA-HJ-NP-Z0-9]{33}$',
+    'BEP20': '^0x[a-fA-F0-9]{40}$',
+    'ERC20': '^0x[a-fA-F0-9]{40}$',
+    'OPERA': '^0x[a-fA-F0-9]{40}$',
+    'FUSE': '^0x[a-fA-F0-9]{40}$',
   }
-  return response.json();
+
+  return versionToRegexMap[version];
 }
 
-const delay = (ms: number) => {
-  return new Promise( resolve => setTimeout(resolve, ms) );
+const saveUpdates = (updatedKeys: {
+  [key in string]: {
+    deprecatedKeyName: string, validationRegex: string | null, deprecated: boolean
+  }}) => {
+  const versionParts = SupportedKeys.version.split('.');
+  versionParts[2] = (Number(versionParts[2]) + 1).toString();
+  const version = versionParts.join('.');
+  const newFile = {
+    version: version,
+    keys: updatedKeys
+  };
+  fs.writeFileSync("./src/supported-keys/supported-keys.json", JSON.stringify(newFile));
 }
 
 main();
